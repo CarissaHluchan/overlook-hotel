c41b0c33dfac6880015d107789ed4d16dc37c52c
Carissa Hluchan <carissahluchan@gmail.com>
2024-07-31 16:40:18 -0600

    Add: set up functions and testing file

---
+++ test/tests.js
@@ -0,0 +1,12 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+import { bookingsSampleData } from '../src/data-sample/bookings-sample';
+import { roomsSampleData } from '../src/data-sample/rooms-sample';
+import { customersSampleData  } from '../src/data-sample/customer-sample';
+
+describe('See if the tests are running', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+});
