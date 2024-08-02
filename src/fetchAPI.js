6a574f654a15e2a2c3c83b7a4745ab4287263f6c
Carissa Hluchan <carissahluchan@gmail.com>
2024-08-01 20:09:06 -0600

    Add: function names and outline for fetch API calls

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -19,8 +19,30 @@ export const fetchUsers = () => {
         .catch(error => console.log(error))
 }
 
-export const addRoomToBookings = () => { }
+export const addRoomToBookings = (booking) => {
+    fetch('http://localhost:3001/api/v1/bookings', {
+        method: 'POST',
+        body: JSON.stringify(booking),
+    })
 
-export const deleteRoomFromBookings = () => { }
+    .then(response => response.json())
+    .then(json => /*do something with json*/)
+    .catch(err => /*do something with the error*/)
+
+    /**Required Properties for Request: { "userID": 48, "date": "2019/09/23", "roomNumber": 4 }
+     * Sample Successful Response: { message: 'Booking with id <id> successfully posted', newBooking: <Object with trip info just posted> }
+     */
+}
+
+export const deleteRoomFromBookings = () => {
+    fetch(`http://localhost:3001/api/v1/bookings/${booking.id}`/* where<id> will be a number of a booking’s id*/, {
+        method: 'POST',
+      })
+        .then(response => response.json())
+        .then(json => /*do something with json*/)
+        .catch(err => /*do something with the error*/);
+
+        /**Sample Successful Response: { message: Booking #<id> has been deleted }*/
+}
 
 
