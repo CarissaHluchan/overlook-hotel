d182f4bafbf2b637413a74c14d68369e6c79ada8
Carissa Hluchan <162339081+CarissaHluchan@users.noreply.github.com>
2024-08-06 13:47:28 -0600

    Update fetchAPI.js
    navigation to Vercel Overlook API host: https://overlook-api-jmst.vercel.app

--- src/fetchAPI.js
+++ src/fetchAPI.js
@@ -1,23 +1,23 @@
 export function fetchRooms() {
-    return fetch('http://localhost:3001/api/v1/rooms')
+    return fetch('https://overlook-api-jmst.vercel.app/api/v1/rooms')
         .then(response => response.json())
         .then(data => data.rooms);
 }
 
 export function fetchBookings() {
-    return fetch('http://localhost:3001/api/v1/bookings')
+    return fetch('https://overlook-api-jmst.vercel.app/api/v1/bookings')
         .then(response => response.json())
         .then(data => data.bookings);
 }
 
 export function fetchUsers() {
-    return fetch('http://localhost:3001/api/v1/customers')
+    return fetch('https://overlook-api-jmst.vercel.app/api/v1/customers')
         .then(response => response.json())
         .then(data => data.customers);
 }
 
 export function addRoomToBookings(userId, roomNumber, date) {
-    return fetch('http://localhost:3001/api/v1/bookings', {
+    return fetch('https://overlook-api-jmst.vercel.app/api/v1/bookings', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -33,7 +33,7 @@ export function addRoomToBookings(userId, roomNumber, date) {
 }
 
 export function deleteRoomFromBookings(bookingId) {
-    return fetch(`http://localhost:3001/api/v1/bookings/${bookingId}`, {
+    return fetch(`https://overlook-api-jmst.vercel.app/api/v1/bookings/${bookingId}`, {
         method: 'DELETE',
     })
         .then(response => response.json());
